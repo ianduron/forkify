b009bcd13bf34c5be51b98002de1789cd2bdf0b6
Ian <ianduronb@gmail.com>
2019-03-19 17:36:06 -0700

    API calls

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,9 @@
-// Global app controller
- import num from './test';
+import axios from 'axios';
 
- console.log(`I imported ${num} from another module! Variable x is ${x}`); 
+async function getResults(query){
+    const key = 'e681c5c8e235db080481a0ab8c0fed20';
+    const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
+    console.log(res);
+}
+
+getResults('pizza');
