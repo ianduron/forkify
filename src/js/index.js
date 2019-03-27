1302a63ad1cfc01bc7efb2d54bf69d72cc8c7aaa
Ian <ianduronb@gmail.com>
2019-03-26 23:22:52 -0700

    search model

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,30 @@
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getResults(query){
-    const key = 'e681c5c8e235db080481a0ab8c0fed20';
-    const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-    console.log(res);
+const state = {};
+
+const controlSearch = async () => {
+    // 1) Get query from view
+    const query = 'pizza'; //TODO
+
+    if(query){
+        // 2) New search object and add to state
+            state.search = new Search(query);
+
+        // 3) Prepare UI for results
+
+        // 4) Search for recipes
+        await state.search.getResults();
+
+        // 5) Render results on UI
+        console.log(state.search.result);
+    }
 }
 
-getResults('pizza');
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
+const search = new Search('pizza');
+console.log(search);
+search.getResults();
---
+++ src/js/models/Search.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export default class Search{
+    constructor(query){
+        this.query = query;
+    }
+
+    async getResults() {
+        try {
+            const proxy = 'https://cors-anywhere.herokuapp.com/';
+            const key = 'e681c5c8e235db080481a0ab8c0fed20';
+            const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.result = res.data.recipes;
+            // console.log(this.result);
+        } catch (error) {
+            alert(error);
+        }
+    }
+}
