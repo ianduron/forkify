5493ac24c024d94662171802bd39fdbbb3861bdc
Ian <ianduronb@gmail.com>
2019-03-27 14:37:28 -0700

    search view

--- src/js/index.js
+++ src/js/index.js
@@ -1,30 +1,31 @@
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
 
 const state = {};
 
 const controlSearch = async () => {
     // 1) Get query from view
-    const query = 'pizza'; //TODO
+    const query = searchView.getInput(); 
+    console.log(query);
 
     if(query){
         // 2) New search object and add to state
-            state.search = new Search(query);
+        state.search = new Search(query);
 
         // 3) Prepare UI for results
-
+        searchView.clearInput();
+        searchView.clearResults();
         // 4) Search for recipes
         await state.search.getResults();
 
         // 5) Render results on UI
-        console.log(state.search.result);
+        searchView.renderResults(state.search.result);
     }
 }
 
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
 });
 
-const search = new Search('pizza');
-console.log(search);
-search.getResults();
