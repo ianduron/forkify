b14120445c987871c9907feadb0dbc2aeda12319
Ian <ianduronb@gmail.com>
2019-04-24 23:08:52 -0700

    shopping list view

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@ import Recipe from './models/Recipe';
 import List from '.models/List';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
+import * as listView from './views/listView';
 import { elements, renderLoader, clearLoader } from './views/base';
 //import { stat } from 'fs';
 
@@ -93,6 +94,19 @@ const controlRecipe = async() => {
     }
 }
 
+/**
+ * LIST CONTROLLER
+ */
+const controlList = () => {
+    //Create a new list IF there is none yet
+    if(!state.list) state.list = new List();
+
+    //Add each ingredient to the list and UI
+    state.recipe.ingredients.forEach(el => {
+        const item = state.list.addItem(el.count, el.unit, el.ingredient);
+        listView.renderItem(item);
+    });
+}
 
 ['haschange', 'load'].forEach(event => window.addEventListener(event, controlRecipe)); //attaches an event listener to 2 different events
 
@@ -109,8 +123,10 @@ elements.recipe.addEventListener('click', e=> {
         //Decrease button is clicked
         state.recipe.updateServings('inc');
         recipeView.updateServingsIngredients(state.recipe);
-    }}
-);
+    } else if (e.target.matches('.recipe__btn--add .recipe__btn--add *')) {
+        controlList();
+    }
+});
 
 
 
